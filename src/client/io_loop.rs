129d7b7a6a610eddde7bcb71eae373162e94982c
myonn#114 <619563332@qq.com>
2025-03-27 19:51:40 +0800

    Update io_loop.rs

--- src/client/io_loop.rs
+++ src/client/io_loop.rs
@@ -513,7 +513,7 @@ impl<T: InvokeUiSession> Remote<T> {
                     .handle_login_from_ui(os_username, os_password, password, remember, peer)
                     .await;
             }
-            #[cfg(all(target_os = "windows"))]
+            #[cfg(all(target_os = "windows", not(feature = "flutter")))]
             Data::ToggleClipboardFile => {
                 self.check_clipboard_file_context();
             }
@@ -1250,7 +1250,7 @@ impl<T: InvokeUiSession> Remote<T> {
                             }
                         }
                         self.handler.handle_peer_info(pi);
-                        #[cfg(all(target_os = "windows"))]
+                        #[cfg(all(target_os = "windows", not(feature = "flutter")))]
                         self.check_clipboard_file_context();
                         if !(self.handler.is_file_transfer()
                             || self.handler.is_port_forward()
@@ -1977,7 +1977,7 @@ impl<T: InvokeUiSession> Remote<T> {
         true
     }
 
-    #[cfg(all(target_os = "windows"))]
+    #[cfg(all(target_os = "windows", not(feature = "flutter")))]
     fn check_clipboard_file_context(&self) {
         let enabled = *self.handler.server_file_transfer_enabled.read().unwrap()
             && self.handler.lc.read().unwrap().enable_file_copy_paste.v;
